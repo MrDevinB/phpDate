8de3bb16c99792bc5a9cc135c7d2ba084edfa16d
Devin Barth <dbarth@hyc.com>
2013-04-08 13:44:18 -0500

    phpDate javascript

---
+++ phpDate.js
@@ -0,0 +1,103 @@
+function phpDate(str, dt) {
+
+    var out = '',
+        A = new Date(),
+        dt = (typeof dt == 'undefined') ? A : dt,
+        str = (typeof str != 'undefined' && str != '') ? str : 'Y-m-d H:i:s',
+        a = str.split(''),
+        c = dt.getFullYear() / 4,
+        ly = (c == Math.floor(c)),
+        _M = {
+            f: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            s: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            l: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+            c: [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+        },
+        _D = {
+            f: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            s: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        },
+        d = {
+            d: function() { /* Day of the month, 2 digits with leading zeros - 01 to 31 */
+                return ('0' + dt.getDate().toString()).slice(-2);
+            },
+            j: function() { /* Day of the month without leading zeros - 1 to 31*/
+                return dt.getDate();
+            },
+            D: function() { /* A textual representation of a day, three letters - Mon through Sun */
+                return _D.s[dt.getDay()];
+            },
+            l: function() { /* A full textual representation of the day of the week - Sunday through Saturday */
+                return _D.f[dt.getDay()];
+            },
+            w: function() { /* Numeric representation of the day of the week - 0 (for Sunday) through 6 (for Saturday) */
+                return dt.getDay();
+            },
+            z: function() {
+                var B = dt.getMonth() - 1,
+                    C = (B > 0) ? _M.c[B] : 0;
+                C += dt.getDate();
+                C += (ly) ? 1 : 0;
+                return C;
+            },
+            F: function() { /* A full textual representation of a month, such as January or March - January through December */
+                return _M.f[dt.getMonth()];
+            },
+            m: function() { /* Numeric representation of a month, with leading zeros - 01 through 12 */
+                return ('0' + (dt.getMonth() + 1).toString()).slice(-2);
+            },
+            M: function() {
+                return _M.s[dt.getMonth()];
+            },
+            n: function() {
+                return dt.getMonth() + 1;
+            },
+            t: function() {
+                return (dt.getMonth() == 1 && ly) ? '29' : _M.l[dt.getMonth()];
+            },
+            L: function() {
+                return ly;
+            },
+            Y: function() { /* A full numeric representation of a year, 4 digits - Examples: 1999 or 2003 */
+                return dt.getFullYear();
+            },
+            y: function() { /* A two digit representation of a year - Examples: 99 or 03 */
+                return dt.getFullYear().toString().substring(2, 4);
+            },
+            a: function() {
+                return (dt.getHours() >= 11) ? 'pm' : 'am';
+            },
+            A: function() {
+                return (dt.getHours() >= 11) ? 'PM' : 'AM';
+            },
+            g: function() {
+                var A = dt.getHours();
+                return (A > 12) ? A - 12 : (A == 0) ? 12 : A;
+            },
+            G: function() {
+                return dt.getHours();
+            },
+            h: function() {
+                var A = dt.getHours();
+                return ('0' + ((A > 12) ? A - 12 : (A == 0) ? 12 : A).toString()).slice(-2);
+            },
+            H: function() {
+                return ('0' + (dt.getHours()).toString()).slice(-2);
+            },
+            i: function() {
+                return ('0' + (dt.getMinutes()).toString()).slice(-2);
+            },
+            s: function() {
+                return ('0' + (dt.getSeconds()).toString()).slice(-2);
+            },
+            e: function() {
+                log(dt.toString().match(/((\(?)([\w])(\)?))/g));
+                return dt.toString().match(/(\(([\w]?)+\))/g);
+            }
+        };
+
+    for (i = 0; i < a.length; i++)
+        out += (typeof d[a[i]] == 'function') ? d[a[i]]() : a[i];
+
+    return out;
+}
